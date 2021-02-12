369b20b959e2fb5e29c7b3fc4e4244dea3608db0
andreafrank <andreajfrank23@gmail.com>
2021-02-12 16:52:45 -0600

    get lat and long through geolocation, get weather from openweatherapi, display weather

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-// import CurrentWeather from './CurrentWeather';
-import UserGeolocation from './UserGeolocation'
+import CurrentWeather from './CurrentWeather';
 
-const App = () => {
-	const location = UserGeolocation();
-	
-	return (
-		<div className="inline-block mr-auto pt-1">
-                                {location.loaded
-                                    ? JSON.stringify(location)
-                                    : "Location data not available yet."}
-                            </div>
-	)
+const App = () =>  {
+  return (
+    <div>
+      <CurrentWeather />
+    </div>
+  );
 };
 
 export default App;
--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,36 +1,82 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 
-const CurrentWeather = () => {
-	const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
-    const key = 'API_KEY';
-    const [feels_like, setFeelsLike] = useState('');
-    const [mainTemp, setMainTemp] = useState('');
-    const [description, setDescription] = useState('');
-    const [main, setMain] = useState('');
-    const [iconID, setIconID] = useState('');
-   
-    useEffect(()=> {
-		fetch('https://api.openweathermap.org/data/2.5/weather?q=Karachi,pk&APPID={key}&units=imperial')
-		.then(response => response.json())
-		.then(result => {
-		    console.log(result);
-		    setFeelsLike(result.main.feels_like);
-		    setMainTemp(result.main.temp);
-		    setDescription(result.weather[0].description);
-		    setMain(result.weather[0].main);
-		    setIconID(result.weather[0].icon);
-		})
-	},[])
-	
-	return (
-	    <>
-		    <h1>Main Temperature : {mainTemp} °f</h1>
-		    <h1>Feels like: {feels_like} °f</h1>
-		    <h1>Weather Parameter: {main}</h1>
-		    <h1>Description : {description}</h1>
-		    <img src={"http://openweathermap.org/img/wn/" + iconID + "@2x.png"} alt={"Icon representing " + main}/>
-	    </>
-	)	
-};
+const CurrentWeather  = () => {
+	const [lat, setLat] 					= useState(undefined);
+	const [lon, setLon] 					= useState(undefined);
+	const [err, setErr] 					= useState(undefined);
+	const [tempC, setTempC] 				= useState(undefined);
+	const [tempF, setTempF] 				= useState(undefined);
+	const [city, setCity] 					= useState(undefined);
+	const [country, setCountry]				= useState(undefined);
+	const [humidity, setHumidity] 			= useState(undefined);
+	const [description, setDescription] 	= useState(undefined);
+	const [icon, setIcon] 					= useState(undefined);
+	const [sunrise, setSunrise] 			= useState(undefined);
+	const [sunset, setSunset] 				= useState(undefined);
+
+	const getPosition = (options) => {
+	    return new Promise(function (resolve, reject) {
+	      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+	    });
+  	};
+
+ 	const getWeather = async (lat, lon) => {     
+	    const api_call = await fetch(`//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=439e167525fbc2afd365dc0d9d0f40e5&units=metric`);
+	    const data = await api_call.json();
+	    
+	    setLat(lat);
+	    setLon(lon);
+	    setCity(data.name);
+	    setTempC(Math.round(data.main.temp));
+	    setTempF(Math.round(data.main.temp * 1.8 + 32));
+	    setHumidity(data.main.humidity);
+	    setDescription(data.weather[0].description);
+	    setIcon(data.weather[0].icon);
+	    setSunrise(moment.unix(data.sys.sunrise).format('hh:mm a'));
+	    setSunset(moment.unix(data.sys.sunset).format('hh:mm a'));
+  	};
+
+  	useEffect(() => {
+  		// express in ternary
+  		if (navigator.geolocation) {
+	     	getPosition()
+	    	.then((position) => { getWeather(position.coords.latitude, position.coords.longitude )})
+	      	.catch(setErr => err.message);
+    	} else {
+      		alert("Geolocation not available")
+    	};
+
+    	const timerID = setInterval(
+    		() => getWeather(lat, lon),
+      		600000
+    	);  
+
+    	clearInterval(timerID);
+
+    }, []);
+
+    if (city) {
+      	return (
+	        <ul> 
+	        	<div>  
+		          	<li> {city} </li>
+		            <li> {tempC} &deg;C </li>
+		            <li> {tempF} &deg;F</li>            
+		            <li> humidity: {humidity}% </li>
+		            <li> {description} </li>        
+		          	<li> {/*<br /> <img src={`http://openweathermap.org/img/w/${icon}.png`} />*/} </li>
+	          	</div> 
+
+	        	<div>
+		            <li>{sunrise}</li>
+		            <li>{sunset}</li>
+	        	</div>       
+	        </ul>
+    	)
+    } else {
+      	return null;
+    }   
+}
 
 export default CurrentWeather;
